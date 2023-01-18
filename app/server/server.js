ebc7660408cbae8bfcf5200bc55b062f366afac7
Suukoot <Suukootj@gmail.com>
2023-01-18 16:14:56 +0300

    Express routes

--- app/server/server.js
+++ app/server/server.js
@@ -1,35 +1,40 @@
+const { response } = require('express');
 const express = require('express');
-const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const _PORT = 3000;
 
 
+
+//  Homepage
 app.get('/', (req, res)=>{
-    fs.readFile('././resources/views/index.html', (error, returnView)=>{
-        if(error){
-            res.end("Server Error");
-        }
-        else{
-            res.end(returnView);
-        }
-    })
-});
+    res.sendFile('/index.html',{root: '/home/sukkoth/Documents/Workspace/JavaScript/Basics/resources/views'});
+})
 
+//  About page
 app.get('/about', (req, res)=>{
-    fs.readFile('././resources/views/about.html', (error, returnView)=>{
-        if(error){
-            res.end("Server Error");
-        }
-        else{
-            res.end(returnView);
-        }
-    })
+    res.sendFile('/about.html', {root: '/home/sukkoth/Documents/Workspace/JavaScript/Basics/resources/views'});
+})
+
+//login page
+
+app.get('/login', (req, res)=>{
+    res.sendFile('/login.html', {root:'/home/sukkoth/Documents/Workspace/JavaScript/Basics/resources/views'});
+})
+
+//login check credential (but does not work now)
+app.post('/login', (req, res) => {
+    console.log(req);
+    res.redirect('/login');
 });
 
-app.get('/user/:user_name', (req, res) => {
-    res.send(req.params.user_name);
-});
 
-app.listen(PORT, ()=>{
-    console.log(`Server started on port ${PORT}`);
+//  incase user inputs unknown routes, 404 page is shown
+app.use((req, res)=>{
+    res.status(404).sendFile('/404.html', {root: '/home/sukkoth/Documents/Workspace/JavaScript/Basics/resources/views'});
+})
+
+//  Starts the server on port @var _PORT
+
+app.listen(_PORT, ()=>{
+    console.log(`Server started on port ${_PORT}`);
 })
