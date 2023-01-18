a307000e15db454bbec66bd0367dda9b7cd78ae6
Suukoot <Suukootj@gmail.com>
2023-01-18 18:31:56 +0300

    on my own

--- app/server/server.js
+++ app/server/server.js
@@ -1,52 +1,29 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
-
-
-//register ejs
-app.set('view engine', 'ejs');
-//set the folder for your views
-app.set('views', 'resources/views');
 
 const _PORT = 3000;
 
-//  Homepage
+//  Register ejs and it's location for view files
+app.set('view engine', 'ejs');
+app.set('views', 'resources/views');
+
 app.get('/', (req, res)=>{
-    const blogs = [
-        {title: "The dark knight", text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo obcaecati sunt, ad laboriosam aliquid molestiae alias asperiores adipisci libero accusamus dicta architecto, id quia ratione maxime provident, laborum iste. Quaerat reiciendis exercitationem omnis maiores repellat non maxime dolorum cupiditate,"},
-        {title: "Up and Down in the sky", text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo obcaecati sunt, ad laboriosam aliquid molestiae alias asperiores adipisci libero accusamus dicta architecto, id quia ratione maxime provident, laborum iste.Quaerat reiciendis exercitationem omnis maiores repellat non maxime dolorum cupiditate,"},
-        {title: "The love and the poison", text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo obcaecati sunt, ad laboriosam aliquid molestiae alias asperiores adipisci libero accusamus dicta architecto, id quia ratione maxime provident, laborum iste.Quaerat reiciendis exercitationem omnis maiores repellat non maxime dolorum cupiditate,"},
-        {title: "Me, myself and mine", text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quo obcaecati sunt, ad laboriosam aliquid molestiae alias asperiores adipisci libero accusamus dicta architecto, id quia ratione maxime provident, laborum iste.Quaerat reiciendis exercitationem omnis maiores repellat non maxime dolorum cupiditate,"}
-    ]
-    res.render('index', {title: 'Home', blogs:blogs});
+    res.render('index', {title: 'Home'});
 });
 
-//  About page
 app.get('/about', (req, res)=>{
     res.render('about', {title: 'About'});
 });
 
 app.get('/blogs/create', (req, res)=>{
     res.render('create', {title: 'Create'});
-})
-
-//  login page
-app.get('/login', (req, res)=>{
-    res.render('login');
 });
 
-//  login check credential (but does not work now)
-app.post('/login', (req, res) => {
-    console.log(req);
-    res.redirect('/login');
-});
-
-//  Incase user inputs unknown routes, 404 page is shown
 app.use((req, res)=>{
     res.status(404).render('404', {title: '404'});
-});
+})
 
-//  Starts the server on port @var _PORT
 app.listen(_PORT, ()=>{
     console.log(`Server started on port ${_PORT}`);
 });
-
